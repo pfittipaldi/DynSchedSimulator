f8bfc3e2ecef6449df944e13d3e3e363373c7da0
root <root@kadeploy.common.lip6.fr>
2022-05-17 11:16:47 +0200

    AutoRun finished at mar. 17 mai 2022 11:16:47 CEST

--- 2-MWPK/Sim_inputs.py
+++ 2-MWPK/Sim_inputs.py
@@ -1,7 +1,8 @@
+import numpy as np
 PhotonLifeTime = 10e-6 #s, average photon lifetime inside the quantum memories. Should be always higher than time step.
                        # Set to "Inf" for lossless simulation
 t_step = 1e-6; # Length of the time step, s
-time_steps = int(1e3); # Number of steps to simulate per pixel
+time_steps = int(1e4); # Number of steps to simulate per pixel
 memo_len=int(time_steps/2) # How many configurations should be memoized - this refers to the memoization inside the pixels
 beta = 1  # Demand weight in the scheduling calculation     
 
@@ -17,7 +18,7 @@ topologyname = "5 Links Chain" # Just for the plot, type whatever you want here
 
 routes = ['ABCDE','BCDEF'] # Routes to serve the service pairs. Notice that this is separate from the SPair variables, so that several routes may be specified for a single pair.
 
-n_points = 33 # Number of pixels for the stability plot
+n_points = 65 # Number of pixels for the stability plot
 
 SPair_1 = ("A","E") #Service pairs
 SPair_2 = ("B","F")
@@ -25,7 +26,7 @@ SPair_2 = ("B","F")
 DemRates1 = np.linspace(1,1000000,n_points) # points along the first and second pair directions
 DemRates2 = np.linspace(1,1000000,n_points)
 
-Plot200Diag = False
+PlotDiag = True
 
 ParallelRun = True    # Huge performance gain if True. This should be set to False only for debugging reasons (multiprocessing does not support breakpoints)
 
---
+++ 2-MWPK/autorun.sh
@@ -0,0 +1,5 @@
+python ParSweep.py
+git add *
+git commit -m "AutoRun finished at $(date)"
+git push
+
